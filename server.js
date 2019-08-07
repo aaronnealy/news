d20b60676f2db8578cb343f7af7733f91f7703a5
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-08-07 09:39:25 -0500

    "removed handlebars"

---
+++ server.js
@@ -0,0 +1,78 @@
+// Require dependencies 
+var express = require("express");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+//Require all models
+var db = require("./models");
+
+//Change to heroku port later !!!!!!1
+var PORT = 4000; 
+
+var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+//Routes
+
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("http://www.echojs.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article h2").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
+  });
+  
+  // Route for getting all Articles from the db
+  app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+
+      res.render("index", {dbArticle})
+  });
+
+  app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
